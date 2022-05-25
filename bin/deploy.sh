b022fb0699a369e813bd36eb114dc8d18f71a422
Kevin Colyar <kevin@colyar.net>
2022-05-25 16:09:21 -0700

    Exports KUBECONFIG when deploying

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -8,4 +8,5 @@ docker tag latest registry.digitalocean.com/kevincolyar-sensors/latest
 docker push registry.digitalocean.com/kevincolyar-sensors/latest
 
 # Rollout deployment
+export KUBECONFIG=./k8/k8s-1-22-8-do-1-sfo3-1653447631981-kubeconfig.yml
 kubectl rollout restart deployments sensors-deploy
