999c8dc7285b684aa50036388745bb56ba8567fe
Kevin Colyar <kevin@colyar.net>
2022-05-25 21:52:29 -0700

    Adds measurements upsert stored procedure, still needs work

--- db/schema.sql
+++ db/schema.sql
@@ -27,3 +27,13 @@ AS $BODY$
     INSERT INTO measurements(type, device_id, value, created_at)
     VALUES(_type, _device_id, _value, _created_at);
 $BODY$;
+
+CREATE PROCEDURE measurements_upsert(_type varchar, _device_id int, _value double precision, _created_at timestamp)
+LANGUAGE SQL
+AS $BODY$
+    INSERT INTO measurements(type, device_id, value, created_at)
+    VALUES(_type, _device_id, _value, _created_at)
+    ON CONFLICT(created_at, device_id, type)
+    DO
+    UPDATE SET value = _value
+$BODY$;
